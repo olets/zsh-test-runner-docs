d0afe918b4de866a081364cd5cc76c143a3749f9
Henry Bley-Vroman <olets@olets.dev>
2023-09-11 18:19:52 -1000

    feat(favicon): add

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,63 @@ import starlight from '@astrojs/starlight';
 export default defineConfig({
   integrations: [
     starlight({
+      head: [
+        {
+          tag: 'link',
+          attrs: {
+            rel: "apple-touch-icon" ,
+            sizes: "180x180" ,
+            href: "/apple-touch-icon.png"
+          },
+        },
+        {
+          tag: 'link',
+          attrs: {
+            rel: "icon" ,
+            type: "image/png" ,
+            sizes: "32x32" ,
+            href: "/favicon-32x32.png"
+          },
+        },
+        {
+          tag: 'link',
+          attrs: {
+            rel: "icon" ,
+            type: "image/png" ,
+            sizes: "16x16" ,
+            href: "/favicon-16x16.png"
+          },
+        },
+        {
+          tag: 'link',
+          attrs: {
+            rel: "manifest" ,
+            href: "/site.webmanifest"
+          },
+        },
+        {
+          tag: 'link',
+          attrs: {
+            rel: "mask-icon" ,
+            href: "/safari-pinned-tab.svg" ,
+            color: "#5bbad5"
+          },
+        },
+        {
+          tag: 'meta',
+          attrs: {
+            name: "msapplication-TileColor",
+            content: "#ffffff",
+          },
+        },
+        {
+          tag: 'meta',
+          attrs: {
+            name: "theme-color",
+            content: "#ffffff",
+          },
+        },
+      ],
       title: 'zsh-test-runner',
       social: {
         github: 'https://github.com/olets/zsh-test-runner',
