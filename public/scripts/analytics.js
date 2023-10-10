afdb19e54178ee0586a529b686319dcc920774d6
Henry Bley-Vroman <olets@olets.dev>
2023-10-09 15:01:17 -1000

    feat(analytics): track search...
    Tracks opening the search dialog,
    closing the search dialog,
    and clicking on search hits.

--- public/scripts/analytics.js
+++ public/scripts/analytics.js
@@ -49,7 +49,88 @@ function trackLinks() {
   }
 }
 
-if (window?.fathom?.trackEvent) {
+function trackSearch() {
+  const searchDialogs = Array.from(document.querySelectorAll("dialog[aria-label='Search']"));
+
+  const hits = document.getElementsByClassName("pagefind-ui__result-link");
+
+  function trackHits(hits) {
+    for (const hit of hits) {
+      
+      hit.addEventListener("click", ({ target }) => {
+        const title = target.innerText || '';
+        const query = target.closest('.pagefind-ui__form').querySelector('.pagefind-ui__search-input').value;
+
+        window.fathom.trackEvent("Search hit clicked", {
+          query: query,
+          title: title,
+        });
+      });
+    }
+  }
+  
+  function trackSearchDialogs(searchDialogs) {
+    for (const searchDialog of searchDialogs) {
+      searchDialog.addEventListener('open', () => {
+        window.fathom.trackEvent("Search dialog opened");
+      })
+      
+      searchDialog.addEventListener('close', () => {
+        const query = searchDialog.querySelector('.pagefind-ui__search-input').value;
+
+        window.fathom.trackEvent("Search dialog closed", { query: query });
+      })
+    }
+  }
+  
+  const searchObserver = new MutationObserver((mutationList) => {
+    const addedNodes = mutationList.flatMap((mutationRecord) => {
+      return Array.from(mutationRecord.addedNodes)
+    })
+    
+    const hits = addedNodes.filter((addedNode) => {
+      return addedNode.classList?.contains('pagefind-ui__result-link')
+    })
+
+    const searchDialogs = addedNodes.filter((addedNode) => {
+      return addedNode.tagName === "DIALOG" && addedNode.getAttribute("aria-label") === "Search";
+    })
+
+    trackHits(hits);
+
+    trackSearchDialogs(searchDialogs);
+  });
+  
+  searchObserver.observe(document, {
+    childList: true,
+    subtree: true,
+  });
+  
+  trackHits(hits);
+  trackSearchDialogs(searchDialogs);
+}
+
+function addOpenEventToDialogs() {
+  const dialogs = document.getElementsByTagName("dialog");
+
+  const openEvent = new CustomEvent("open");
+
+  const observer = new MutationObserver((mutationList) => {
+    mutationList.forEach(({ attributeName, target }) => {
+      if (attributeName === "open" && target.open) {
+        target.dispatchEvent(openEvent);
+      }
+    });
+  });
+
+  for (dialog of dialogs) {
+    observer.observe(dialog, { attributes: true });
+  }
+}
+
+// if (window?.fathom?.trackEvent) {
+  addOpenEventToDialogs();
   addTrackingToNav();
   trackLinks();
-}
+  trackSearch();
+// }
