401cf15324e91150d5b3c5488cc8587e0f07d5a0
Henry Bley-Vroman <henry.bley-vroman@viget.com>
2025-10-15 17:21:20 -1000

    feat(analytics): support umami

--- public/scripts/analytics.js
+++ public/scripts/analytics.js
@@ -2,33 +2,41 @@ function addTrackingToNav() {
   const links = [
     {
       els: Array.from(
-        document.querySelectorAll('.sidebar a[href="https://github.com/olets/zsh-test-runner/blob/main/CONTRIBUTING.md"]')
+        document.querySelectorAll(
+          '.sidebar a[href="https://github.com/olets/zsh-test-runner/blob/main/CONTRIBUTING.md"]',
+        ),
       ),
-      fathomEventId: "Click Sidebar Contributing",
+      trackedEventId: "Click Sidebar Contributing",
     },
     {
       els: Array.from(
-        document.querySelectorAll('.sidebar a[href="https://github.com/olets/zsh-test-runner/blob/main/CHANGELOG.md"]')
+        document.querySelectorAll(
+          '.sidebar a[href="https://github.com/olets/zsh-test-runner/blob/main/CHANGELOG.md"]',
+        ),
       ),
-      fathomEventId: "Click Sidebar Changelog",
+      trackedEventId: "Click Sidebar Changelog",
     },
     {
       els: Array.from(
-        document.querySelectorAll('.sidebar a[href="https://github.com/olets/zsh-test-runner/blob/main/LICENSE"]')
+        document.querySelectorAll(
+          '.sidebar a[href="https://github.com/olets/zsh-test-runner/blob/main/LICENSE"]',
+        ),
       ),
-      fathomEventId: "Click Sidebar License",
+      trackedEventId: "Click Sidebar License",
     },
     {
       els: Array.from(
-        document.querySelectorAll('.header a[href="https://github.com/olets/zsh-test-runner"]')
+        document.querySelectorAll(
+          '.header a[href="https://github.com/olets/zsh-test-runner"]',
+        ),
       ),
-      fathomEventId: "Click Sidebar Repo",
+      trackedEventId: "Click Sidebar Repo",
     },
   ];
 
   for (const link of links) {
     link.els.forEach((el) =>
-      el.setAttribute("data-fathom-event-id", link.fathomEventId)
+      el.setAttribute("data-track-event-id", link.trackedEventId),
     );
   }
 }
@@ -37,75 +45,91 @@ function trackLinks() {
   const links = document.getElementsByTagName("a");
 
   for (const link of links) {
-    const fathomEventId = link.getAttribute("data-fathom-event-id");
+    const trackedEventId = link.getAttribute("data-track-event-id");
 
-    if (!fathomEventId) {
+    if (!trackedEventId) {
       continue;
     }
 
     link.addEventListener("click", () => {
-      window.fathom.trackEvent(fathomEventId);
+      window?.fathom?.trackEvent(trackedEventId);
+      window?.umami?.track(trackedEventId.substring(0, 50));
     });
   }
 }
 
 function trackSearch() {
-  const searchDialogs = Array.from(document.querySelectorAll("dialog[aria-label='Search']"));
+  const searchDialogs = Array.from(
+    document.querySelectorAll("dialog[aria-label='Search']"),
+  );
 
   const hits = document.getElementsByClassName("pagefind-ui__result-link");
 
   function trackHits(hits) {
     for (const hit of hits) {
-      
       hit.addEventListener("click", ({ target }) => {
-        const title = target.innerText || '';
-        const query = target.closest('.pagefind-ui__form').querySelector('.pagefind-ui__search-input').value;
+        const title = target.innerText || "";
+        const query = target
+          .closest(".pagefind-ui__form")
+          .querySelector(".pagefind-ui__search-input").value;
 
-        window.fathom.trackEvent("Search hit clicked", {
+        window?.fathom?.trackEvent("Search hit clicked", {
+          query: query,
+          title: title,
+        });
+
+        window?.umami?.track("Search hit clicked", {
           query: query,
           title: title,
         });
       });
     }
   }
-  
+
   function trackSearchDialogs(searchDialogs) {
     for (const searchDialog of searchDialogs) {
-      searchDialog.addEventListener('open', () => {
-        window.fathom.trackEvent("Search dialog opened");
-      })
-      
-      searchDialog.addEventListener('close', () => {
-        const query = searchDialog.querySelector('.pagefind-ui__search-input').value;
+      searchDialog.addEventListener("open", () => {
+        window?.fathom?.trackEvent("Search dialog opened");
+        window?.umami?.track("Search dialog opened");
+      });
 
-        window.fathom.trackEvent("Search dialog closed", { query: query });
-      })
+      searchDialog.addEventListener("close", () => {
+        const query = searchDialog.querySelector(
+          ".pagefind-ui__search-input",
+        ).value;
+
+        window?.fathom?.trackEvent("Search dialog closed", { query: query });
+        window?.umami?.track("Search dialog closed", { query: query });
+      });
     }
   }
-  
+
   const searchObserver = new MutationObserver((mutationList) => {
     const addedNodes = mutationList.flatMap((mutationRecord) => {
-      return Array.from(mutationRecord.addedNodes)
-    })
-    
+      return Array.from(mutationRecord.addedNodes);
+    });
+
     const hits = addedNodes.filter((addedNode) => {
-      return addedNode.classList?.contains('pagefind-ui__result-link')
-    })
+      return addedNode.classList?.contains("pagefind-ui__result-link");
+    });
 
     const searchDialogs = addedNodes.filter((addedNode) => {
-      return addedNode.tagName === "DIALOG" && addedNode.getAttribute("aria-label") === "Search";
-    })
+      return (
+        addedNode.tagName === "DIALOG" &&
+        addedNode.getAttribute("aria-label") === "Search"
+      );
+    });
 
     trackHits(hits);
 
     trackSearchDialogs(searchDialogs);
   });
-  
+
   searchObserver.observe(document, {
     childList: true,
     subtree: true,
   });
-  
+
   trackHits(hits);
   trackSearchDialogs(searchDialogs);
 }
@@ -129,8 +153,8 @@ function addOpenEventToDialogs() {
 }
 
 // if (window?.fathom?.trackEvent) {
-  addOpenEventToDialogs();
-  addTrackingToNav();
-  trackLinks();
-  trackSearch();
+addOpenEventToDialogs();
+addTrackingToNav();
+trackLinks();
+trackSearch();
 // }
